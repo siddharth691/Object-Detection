7b2cfc50576d98f4b832d6038acf1a4b656bd4d3
Siddharth Agarwal <siddharth691@gmail.com>
2018-06-17 17:37:43 -0400

    Edit config.py for remote server

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 #Config file
 
-dir_path = "/home/siddharth/Desktop/Adversarial Learning SP/DL/object_detection/data"
-log_file_path = "/home/siddharth/Desktop/Adversarial Learning SP/DL/object_detection/log"
-checkpoint_path = "/home/siddharth/Desktop/Adversarial Learning SP/DL/object_detection/checkpoint"
+dir_path = "/home/sagarwal311/practice_dl/object_detection_traffic_sign/data"
+log_file_path = "/home/sagarwal311/practice_dl/object_detection_traffic_sign/log"
+checkpoint_path = "/home/sagarwal311/practice_dl/object_detection_traffic_sign/checkpoint"
 checkpoint = 200
 
-image_size = 256
+image_size = 1024
 image_width = 2048 
 image_height = 2048
 
-dataset_ratio = 0.1
-batch_size = 100
+dataset_ratio = 1
+batch_size = 200
 no_boxes_per_cell = 7
 no_grid = 10
 no_classes = 221
@@ -20,7 +20,7 @@ confidence_obj_scale = 0.9
 confidence_noobj_scale = 0.4
 coord_scale = 0.8
 
-epoch = 20
+epoch = 100
 learning_rate = 0.0000005
 alpha = 0.1
 dropout = 0.2
