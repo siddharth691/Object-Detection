65c5fc5e3e3aab3e15a78659de384442d6d94c66
Siddharth Agarwal <siddharth691@gmail.com>
2018-06-26 10:35:06 -0400

    Tune params

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ checkpoint_path = "/home/sagarwal311/practice_dl/object_detection_traffic_sign/c
 pretrained_model_path = "/home/sagarwal311/practice_dl/object_detection_traffic_sign/yolo.ckpt"
 checkpoint = 200
 
-image_size = 256
+image_size = 448
 image_width = 2048 
 image_height = 2048
 
-dataset_ratio = 0.5
-batch_size = 30
+dataset_ratio = 1
+batch_size = 20
 no_boxes_per_cell = 7
 no_grid = 10
 no_classes = 221
@@ -21,7 +21,7 @@ confidence_obj_scale = 0.9
 confidence_noobj_scale = 0.4
 coord_scale = 0.8
 
-epoch = 100
-learning_rate = 0.00025
+epoch = 300
+learning_rate = 0.0001
 alpha = 0.1
 dropout = 0.2
