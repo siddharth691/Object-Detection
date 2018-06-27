7b7837c694cd808b54bee4790fb2e16876dd0be8
Siddharth Agarwal <siddharth691@gmail.com>
2018-06-26 23:14:36 -0400

    Fix a bug

--- mem_plot.sh
+++ mem_plot.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Memory\n"
 
-end=$((SECONDS+60))
+end=$((SECONDS+40500))
 while [ $SECONDS -lt $end ]; do
-MEMORY=$(top -bn1 | grep 26865 | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+MEMORY=$(top -bn1 | grep 23479 | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
 echo "$MEMORY"
-sleep 5
+sleep 405
 done
