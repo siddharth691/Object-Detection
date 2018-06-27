f424ec0563c86ebf4b4375794e896bccd11ec8d5
Siddharth Agarwal <siddharth691@gmail.com>
2018-06-26 23:33:28 -0400

    Add feature to extract from any pid no

--- mem_plot.sh
+++ mem_plot.sh
@@ -3,7 +3,7 @@ printf "Memory\n"
 
 end=$((SECONDS+40500))
 while [ $SECONDS -lt $end ]; do
-MEMORY=$(top -bn1 | grep 23479 | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+MEMORY=$(top -bn1 | grep sagarwa+ | awk '{printf "%.2f%%\t\t\n", $(NF-2)}' | awk 'NR==1{printf "%.2f%%\t\t", $1}')
 echo "$MEMORY"
 sleep 405
 done
