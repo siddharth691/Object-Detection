315f49a8f50e9fc990570a897768f7bf51d10d6a
Siddharth Agarwal <siddharth691@gmail.com>
2018-07-05 19:37:12 -0400

    Debugging

--- config_local.py
+++ config_local.py
@@ -6,22 +6,22 @@ checkpoint_path = "/home/siddharth/Desktop/Adversarial Learning SP/DL/object_det
 pretrained_model_path = "/home/siddharth/Desktop/Adversarial Learning SP/DL/object_detection/yolo.ckpt"
 checkpoint = 200
 
-image_size = 448
+image_size = 1024
 image_width = 2048 
 image_height = 2048
 
-dataset_ratio = 1
-batch_size = 20
+dataset_ratio = 0.01
+batch_size = 2
 no_boxes_per_cell = 7
 no_grid = 10
 no_classes = 221
 
-class_scale = 2.0
-confidence_obj_scale = 2.0
-confidence_noobj_scale = 1.0
+class_scale = 1.0
+confidence_obj_scale = 1.0
+confidence_noobj_scale = 0.5
 coord_scale = 5.0
 
 epoch = 300
-learning_rate = 0.0001
+learning_rate = 0.001
 alpha = 0.1
 dropout = 0.5
